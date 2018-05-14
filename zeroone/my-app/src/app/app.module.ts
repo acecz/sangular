93ca18a6dfb6c818d1e4d3d4368822e2cabaf7f3
alwyntso <alwyntso@gmail.com>
2018-05-14 18:19:02 +0800

    update with error

--- zeroone/my-app/src/app/app.module.ts
+++ zeroone/my-app/src/app/app.module.ts
@@ -2,7 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryTodoDbService } from './todo/todo-data';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
@@ -22,7 +23,8 @@ import { TodoComponent } from './todo/todo.component';
     BrowserModule,
     FormsModule,
     HttpClientModule,
-    routing
+    routing,
+    InMemoryWebApiModule.forRoot(InMemoryTodoDbService)
   ],
   providers: [
     { provide: 'auth', useClass: AuthService }
--- zeroone/my-app/src/app/todo/todo.service.ts
+++ zeroone/my-app/src/app/todo/todo.service.ts
@@ -1,20 +1,28 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { UUID } from 'angular2-uuid';
+import 'rxjs/add/operator/toPromise';
 import { Todo } from './todo.model';
 
 @Injectable()
 export class TodoService {
+  private api_url = 'api/todos';
+  private headers = new HttpHeaders({ 'Content-Type': 'application/json' });
   todos: Todo[] = [];
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
-  addTodo(todoItem:string): Todo[] {
+  addTodo(todoItem: string): Promise<Todo> {
     let todo = {
       id: UUID.UUID(),
       desc: todoItem,
       completed: false
     };
-    this.todos.push(todo);
-    return this.todos;
+    return this.http.post(this.api_url, JSON.stringify(todo), { headers: this.headers }).toPromise().
+      then().catch(this.handleError);
+  }
+  private handleError(error: any): Promise<any> {
+    console.error('An error occurred', error);
+    return Promise.reject(error.message || error);
   }
 }
