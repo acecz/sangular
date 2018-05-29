7100e47c35c528be29dda23f33fbb8fb5182b2e6
alwyntso <alwyntso@outlook.com>
2018-05-29 03:29:51 -0800

    init angular-hello-world

--- ngbook/angular-hello-world/src/app/app.module.ts
+++ ngbook/angular-hello-world/src/app/app.module.ts
@@ -1,15 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
-import { HelloWorldComponent } from './hello-world/hello-world.component';
-
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HelloWorldComponent
+    AppComponent
   ],
   imports: [
     BrowserModule
